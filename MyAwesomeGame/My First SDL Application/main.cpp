e5411612fea24b38f0b636a5cbfb60d0ecd42003
Lauren Montenegro <laurenmj187@gmail.com>
2020-02-21 08:42:35 +0100

    Update main.cpp

--- MyAwesomeGame/My First SDL Application/main.cpp
+++ MyAwesomeGame/My First SDL Application/main.cpp
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 
 int main(void) {
 	return 0;
